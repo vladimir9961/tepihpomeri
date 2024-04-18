c2fdb0fd9c11134c6198a0a21df25df1e6dab585
vladimir9961 <vladimir.mil21@gmail.com>
2024-04-18 19:34:22 +0200

    mobile navigation menu update added animation for open and close of menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,14 +10,24 @@ import { socialIcons } from './utils/constants/social-icons.constants';
 // Models
 import { navigationIcons } from './utils/model/navigation-icons.model';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+
+// Animations
+import { fade, slideInOut } from '../../helper/animations/mobile.animation';
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, AngularSvgIconModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
+  animations: [slideInOut, fade]
 
 })
 export class NavbarComponent{
-  public icons: navigationIcons[] = socialIcons.SOCIAL_ICONS
+  public icons: navigationIcons[] = socialIcons.SOCIAL_ICONS;
+
+  public isMobileMenuOpen: boolean = false;
+
+  public toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
---
+++ src/app/helper/animations/mobile.animation.ts
@@ -0,0 +1,21 @@
+import { trigger, style, animate, transition } from '@angular/animations';
+
+export const slideInOut = trigger('slideInOut', [
+  transition(':enter', [
+    style({ transform: 'translateX(100%)' }),
+    animate('0.3s ease-in', style({ transform: 'translateX(0%)' }))
+  ]),
+  transition(':leave', [
+    animate('0.3s ease-out', style({ transform: 'translateX(100%)', width: '100%' }))
+  ])
+]);
+
+export const fade = trigger('fade', [
+    transition(':enter', [
+      style({ opacity: 0 }), // initial state
+      animate('1s ease', style({ opacity: 1 })) // final state
+    ]),
+    transition(':leave', [
+      animate('0s ease', style({ opacity: 0 })) // final state
+    ])
+  ]);
